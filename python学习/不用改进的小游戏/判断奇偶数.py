cf3d2ceba85db064270e12263575add8919c2975
huangsw77777 <864703847@qq.com>
2018-06-19 20:01:55 +0800

    判断奇偶数，打印某段数字中的奇数
    1

--- python学习/不用改进的小游戏/判断奇偶数.py
+++ python学习/不用改进的小游戏/判断奇偶数.py
@@ -1,7 +1,8 @@
 temp = input('输入一个数字，判断奇偶数:')
-while temp.isdigit():
+while not temp.isdigit():
     temp = input('请输入一个整数:')
-if(temp % 2 = 1):
+number = int(temp)
+if(number % 2 == 1):
     print('这是奇数')
 else:
     print('这是偶数')
---
+++ python学习/不用改进的小游戏/打印0~100的奇数.py
@@ -0,0 +1,8 @@
+i = 100
+while i >= 0:
+    if (i % 2 ==1):
+        print(i,end = ' ')
+        i -= 1
+    else:
+        i -=1
+    
