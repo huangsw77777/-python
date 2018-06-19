69545449b0a13911e6d37e2d5d914c4fac85a193
huangsw77777 <864703847@qq.com>
2018-06-19 18:45:23 +0800

    1
    1

---
+++ python学习/判断奇偶数.py
@@ -0,0 +1,7 @@
+temp = input('输入一个数字，判断奇偶数:')
+while temp.isdigit():
+    temp = input('请输入一个整数:')
+if(temp % 2 = 1):
+    print('这是奇数')
+else:
+    print('这是偶数')
---
+++ python学习/测试.py
@@ -0,0 +1,4 @@
+temp = input('输入一个数字锕：')
+while not temp.isdigit():
+    print('不是一个整数')
+print('这是一个整数')
